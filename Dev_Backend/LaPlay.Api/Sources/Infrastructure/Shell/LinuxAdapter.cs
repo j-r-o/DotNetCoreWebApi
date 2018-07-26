d05135ed6fe3f3cfbe91bae8082bdbeaa20f27b4
Julien Rocco <julien.rocco.prestataire@gouv.nc>
2018-07-26 17:04:44 +1100

    wip synchro

--- Dev_Backend/LaPlay.Api/Sources/Infrastructure/Shell/LinuxAdapter.cs
+++ Dev_Backend/LaPlay.Api/Sources/Infrastructure/Shell/LinuxAdapter.cs
@@ -29,13 +29,28 @@ namespace LaPlay.Infrastructure.Shell
             process.WaitForExit();
             return result;
         }
+
+        public Process RunNeverEndingCommand(String command)
+        {
+            var escapedArgs = command.Replace("\"", "\\\"");
+
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            
+            return process;
+        }
     }
 
     // inotifywait -mr -e modify,attrib,moved_to,moved_from,create,delete --format '{"f":"%w", "f":"%f", "f":"%e", "t":"%T"}' --timefmt '%F %T' /tmp
 
-    // dd if=/dev/urandom of=1GB.bin bs=64M count=16 iflag=fullblock
-
-    // lsyncd -delay 0 -nodaemon -rsync /tmp/rsync/a /tmp/rsync/b
-
-    // cat * > merged-file2
 }
---
+++ Dev_Backend/LaPlay.Tests/Sources/Infrastructure/Shell/LinuxAdapterTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Diagnostics;
+
+namespace LaPlay.Infrastructure.Shell
+{
+    public class LinuxAdapterTest
+    {
+        public string RunNeverEndingCommand_ShouldSucceed()
+        {
+
+        }
+    }
+}
