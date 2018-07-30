ecc2178984f65ff14e6a4fdc7a6cb93067b1f98c
Julien Rocco <julien.rocco.prestataire@gouv.nc>
2018-07-31 09:30:41 +1100

    wip listfiles

--- Dev_Backend/LaPlay.Tests/Sources/Infrastructure/Shell/LinuxAdapterTest.cs
+++ Dev_Backend/LaPlay.Tests/Sources/Infrastructure/Shell/LinuxAdapterTest.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Diagnostics;
 using Xunit;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace LaPlay.Infrastructure.Shell
 {
@@ -19,9 +20,21 @@ namespace LaPlay.Infrastructure.Shell
 
             String process = linuxAdapter.RunCommand("tree /media/sf_D_DRIVE/VM -a -f -i -D -F -s --timefmt \"%F %T\"");
 
-            String[] lines = process.Split('\n');
+            // String[] lines = process.Split('\n');
+            // from a in process.Split('\n')
+            // select new new { file = match.Groups[3].Value};
 
-            List<String> lst = lines.OfType<String>().ToList();
+            List<string> lines = new List<string>(process.Split('\n'));
+
+            lines.Select(l => {
+                var match = Regex.Match(l, "^\\[ *([0-9]*) (....-..-.. ..:..:..)]  (.*)$");
+                if(match.Success)
+                {
+                    return "{\"File\":\"" + match.Groups[3].Value + "\", \"ModifiedOn\":\"" + match.Groups[2].Value + "\", \"Bytes\":" + match.Groups[1].Value + "}";
+                }
+                return "";
+            }).Where(l => !String.IsNullOrEmpty(l))
+            .select new { file = match.Groups[3].Value};
 
             var json = from l in lines
                        select new { file = match.Groups[3].Value};
@@ -40,6 +53,7 @@ namespace LaPlay.Infrastructure.Shell
                         tw.WriteLine(b);
                     }
                     //else tw.Write("!!! " + line);
+                    DateTime myDate = DateTime.ParseExact("2009-05-08 14:40:52", "yyyy-MM-dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
                 }
             }
 
